7e15a7d4f09d3d3e89086d6f747c7b9d7518cd5c
unknown <stronka.k@gmail.com>
2019-09-24 08:19:58 +0200

    move i declaration to the outside of the loop

--- src/include/ctestie.h
+++ src/include/ctestie.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #define ASSERT(OPERATION, MESSAGE, ...) snprintf(_buff, 100, MESSAGE, ##__VA_ARGS__); TEST result = {OPERATION, #OPERATION, _buff, __FUNCTION__}; return result
 #define RUN(...) int main(){printf(_C"\n[CTestie] Running tests in: %s\n\n"_N, __FILE__ );TEST result; int passed = 0, run=0; test_function array[] = {__VA_ARGS__, __test_sentinel};\
-for (int i=0; array[i] != __test_sentinel; ++i){_eval(array[i]);} printf(run-passed ? _fail_summary : _success_summary, run, passed, run-passed);return 0;}
+int i;for (i=0; array[i] != __test_sentinel; ++i){_eval(array[i]);} printf(run-passed ? _fail_summary : _success_summary, run, passed, run-passed);return 0;}
 #define _eval(TEST_CASE) result = TEST_CASE(); !(result.result) ? printf("[CTestie:Fail] %s\n Assertion: %s\n Message: %s\n\n", result.function, result.assertion, result.message) : ++passed; ++run
 #define _R "\x1b[31m"
 #define _G "\x1b[32m"
