3b8fe9aac630018be42e1697cd337c05bf8feea6
unknown <stronka.k@gmail.com>
2019-09-25 21:13:36 +0200

    modify ctestie

--- src/include/ctestie.h
+++ src/include/ctestie.h
@@ -1,7 +1,7 @@
 #ifndef __CTESTIE_H__
 #define __CTESTIE_H__
 #include <stdio.h>
-#define ASSERT(OPERATION, MESSAGE, ...) snprintf(_buff, 100, MESSAGE, ##__VA_ARGS__); TEST result = {OPERATION, #OPERATION, _buff, __FUNCTION__}; return result
+#define ASSERT(OPERATION, MESSAGE, ...) snprintf(_buff, 100, MESSAGE, ##__VA_ARGS__); TEST _result = {OPERATION, #OPERATION, _buff, __FUNCTION__}; return result
 #define RUN(...) int main(){printf(_C"\n[CTestie] Running tests in: %s\n\n"_N, __FILE__ );TEST result; int passed = 0, run=0; test_function array[] = {__VA_ARGS__, __test_sentinel};\
 int i;for (i=0; array[i] != __test_sentinel; ++i){_eval(array[i]);} printf(run-passed ? _fail_summary : _success_summary, run, passed, run-passed);return 0;}
 #define _eval(TEST_CASE) result = TEST_CASE(); !(result.result) ? printf("[CTestie:Fail] %s\n Assertion: %s\n Message: %s\n\n", result.function, result.assertion, result.message) : ++passed; ++run
